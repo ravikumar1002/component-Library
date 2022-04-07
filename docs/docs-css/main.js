9c5c38ea7aa142bd1da6c817d74eb46c5bdc9b76
Ravi kumar <1002kumarravi@gmail.com>
2022-04-07 17:07:22 +0530

    add: scroll to top button

--- docs/docs-css/main.js
+++ docs/docs-css/main.js
@@ -1,5 +1,6 @@
 const drawerBtn = document.querySelector(".drawer-btn");
 const drawerContainer = document.querySelector(".aside-drawer-container");
+const scrollToTop = document.querySelector(".scroll-to-top")
 
 drawerBtn.addEventListener("click", () => {
   if (drawerContainer.style.display == "none") {
@@ -8,3 +9,21 @@ drawerBtn.addEventListener("click", () => {
     drawerContainer.style.display = "none";
   }
 });
+
+const scrollFunction = () =>  {
+  if (document.documentElement.scrollTop > 180) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+const topScroll =  () =>  {
+  document.documentElement.scrollTop = 0; 
+}
+
+scrollToTop.addEventListener("click", () => {
+  topScroll()
+})
